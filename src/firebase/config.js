90cc5f03096e55aed1fd00b342077aad372389fa
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-07-28 16:37:01 +0900

    firebase-authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDf7CoQQ4QOm_VTJR3JWmpc2Tq_8AJX4WA",
+  authDomain: "ec-site-c5548.firebaseapp.com",
+  databaseURL: "https://ec-site-c5548.firebaseio.com",
+  projectId: "ec-site-c5548",
+  storageBucket: "ec-site-c5548.appspot.com",
+  messagingSenderId: "637114893040",
+  appId: "1:637114893040:web:190fd5b839969c163d7835",
+  measurementId: "G-X5RZ6YD0H6"
+};
