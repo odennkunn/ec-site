68e09debb5441d2a68b64c6b4333bbaf77838a4b
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-07-28 16:07:52 +0900

    setting-firebase-and-test

---
+++ src/reducks/store/store.js
@@ -0,0 +1,17 @@
+import {createStore as reduxCreateStore, combineReducers, applyMiddleware} from 'redux';
+import {UsersReducer} from '../users/reducers';
+import {connectRouter, routerMiddleware} from 'connected-react-router';
+import thunk from 'redux-thunk';
+
+export default function createStore(history) {
+  return reduxCreateStore(
+    combineReducers({
+      router: connectRouter(history),
+      users: UsersReducer
+    }),
+    applyMiddleware(
+      routerMiddleware(history),
+      thunk
+    )
+  )
+}
